f1d927439d8235b1499b7c0ab812c4402db9eba4
jiayuanyuan <jiayuanyuan11260516@163.com>
2016-11-29 10:43:26 +0800

    updata

---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+var mySwiper=new Swiper('.swiper-content',{
+	direction: 'horizontal',
+	initialSlide: 0,
+	speed:600,
+	loop : true,
+	autoplay : 3000,
+	autoplayDisableOnInteraction : false,
+	nextButton: '.swiper-button-next',
+    prevButton: '.swiper-button-prev',
+	
+	
+});
+$(function(){
+	
+})
