285c58fe3872af990965d163a0030d39d25da2ab
jiayuanyuan <jiayuanyuan11260516@163.com>
2016-11-30 18:53:09 +0800

    updata

--- js/script.js
+++ js/script.js
@@ -1,35 +1,44 @@
-
-$(function(){
-	var mySwiper=new Swiper('.swiper-content',{
-	direction: 'horizontal',
-	initialSlide: 0,
-	speed:600,
-	loop : true,
-	autoplay : 3000,
-	autoplayDisableOnInteraction : false,
-	nextButton: '.swiper-button-next',
-    prevButton: '.swiper-button-prev',
-});
+$(function() {
+	var mySwiper = new Swiper('.swiper-content', {
+		direction: 'horizontal',
+		initialSlide: 0,
+		speed: 600,
+		loop: true,
+		autoplay: 3000,
+		autoplayDisableOnInteraction: false,
+		nextButton: '.swiper-button-next',
+		prevButton: '.swiper-button-prev',
+	});
 })
-window.onload=function(){
-	if($(window).scrollTop()>=500&&$(window).scrollTop()<=700){		
-		if(!$("#logoBig_box").hasClass("rotateInUpRight")){
+window.onload = function() {
+	if($(window).scrollTop() >= 500 && $(window).scrollTop() <= 700) {
+		if(!$("#logoBig_box").hasClass("rotateInUpRight")) {
 			$("#logoBig_box").addClass("displayBox rotateInUpRight");
-		}			
+		}
 	}
-	$(window).scroll(function(){
-		if($(window).scrollTop()>=500&&$(window).scrollTop()<=700){		
-			if(!$("#logoBig_box").hasClass("rotateInUpRight")){
+	$(window).scroll(function() {
+		if($(window).scrollTop() >= 500 && $(window).scrollTop() <= 700) {
+			if(!$("#logoBig_box").hasClass("rotateInUpRight")) {
 				$("#logoBig_box").addClass("displayBox rotateInUpRight");
-			}			
+			}
 		}
 	});
 	
+	
+   var p1 = new Particles();
+
+	p1.init({
+		selector: '#canvas-one',
+		color: '#ffffff',
+		connectParticles: true,
+		maxParticles: 200,
+		minDistance: 25
+	});
 }
 var alert_msg = function(msg) {
-    if ((typeof android !== "undefined" && android !== null) && (android.toast != null)) {
-      return android.toast(msg);
-    } else {
-      return alert(msg);
-    }
+	if((typeof android !== "undefined" && android !== null) && (android.toast != null)) {
+		return android.toast(msg);
+	} else {
+		return alert(msg);
+	}
 };
